ad69c9c9dca8d5edf138a965f7c374920ad18796
elz3r0 <87888538+elz3r0@users.noreply.github.com>
2026-08-13 02:12:58 +0800

    first upload

---
+++ simple_bot.py
@@ -0,0 +1,24 @@
+import logging
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = "8829710593:AAHZTefZtswQMYpK9OLPamOEnp-f9WGCP_Y"
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет, друг! Я твой новый бот. Напиши мне что-нибудь.")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_text = update.message.text
+    await update.message.reply_text(f"Ты написал: {user_text}")
+
+if __name__ == "__main__":
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    print("✅ Бот запущен. Иди в Telegram и напиши ему /start")
+    app.run_polling()
